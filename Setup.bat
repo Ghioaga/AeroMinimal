28dc72c4e8898e9c4f834201f8e9729510e667cd
Andrei Alexandru Popa <alexpopa1997@gmail.com>
2021-09-30 23:17:55 +0300

    Force the dll and registry
    New version for forced dll install and registry

--- Setup.bat
+++ Setup.bat
@@ -1,2 +1,5 @@
 @echo off
 xcopy "%~dp0acpimof.dll" "%windir%\system32\" /Y
+xcopy "%~dp0acpimof.dll" "%windir%\sysWOW64\" /Y
+REG ADD HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Services\WmiAcpi /v MofImagePath /d "%windir%\system32\acpimof.dll" /f
+pause
